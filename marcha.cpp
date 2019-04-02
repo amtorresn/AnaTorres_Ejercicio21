b582af2f3e98a7ba3aacd33bffeba246a9681c3e
amtorresn <am.torresn@uniandes.edu.co>
2019-04-02 12:44:05 +0000

    c4

---
+++ marcha.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+  int i;
+  float r;
+  int n; n = 1000;
+  float x; x=0;
+  float y; y = 0;
+  float pi = 3.1415926539;
+      
+  for (i=0;i<n;i++){
+    r = drand48()*2*pi;
+    x = x + sin(r);
+    y = y + cos(r);
+    std::cout << "Iteración: " << i << " x: " << x << " y: " << y << std::endl;
+  }
+  
+  return 0;
+}
